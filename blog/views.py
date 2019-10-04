f04c659da0a7a91757727062fa484c1b12e0c9d6
Unknown <stafford.carh@gmail.com>
2019-10-04 14:15:47 -0400

    security and / +
    added rudimentary security for creation or editing of posts so those interaction points don't show up if you aren't logged in

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
+from .forms import Postform
+from django.shortcuts import redirect
 from django.shortcuts import render, get_object_or_404
 
 
@@ -14,3 +16,32 @@ def post_detail(request, pk):
     Post.objects.get(pk=pk)
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+
+def post_new(request):
+    if request.method == "POST":
+        form = Postform(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = Postform()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = Postform(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = Postform(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
